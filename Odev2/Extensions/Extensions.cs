30404724f3c1ada12e6c6dfa5511c4b9795e61e9
Burak Kılıç <53341645+burak-kilic@users.noreply.github.com>
2021-12-04 16:17:04 +0300

    Update Extensions

--- Odev2/Extensions/Extensions.cs
+++ Odev2/Extensions/Extensions.cs
@@ -6,10 +6,15 @@ namespace Odev2.Extensions
     {
         public static string LirasToDollars(decimal liras)
         {
+            //Doların değerini sabit değer olarak ata 
             const decimal dollarExcangeRate = 13.73m;
-            var lirasStr = liras.ToString("C2", CultureInfo.CreateSpecificCulture("tr-TR"));
+
             var dollar = liras / dollarExcangeRate;
+
+            //Para birimlerini sembolleriyle birlikte ve ondalıklı olarak göster
+            var lirasStr = liras.ToString("C2", CultureInfo.CreateSpecificCulture("tr-TR"));
             var dollarStr = dollar.ToString("C2", CultureInfo.CreateSpecificCulture("us-US");
+            
             return $"{lirasStr} = {dollarStr}";
         } 
     }
